583cff44632d9b46aa6dcc51917601d6412a9834
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2017-04-21 20:38:50 +0200

    Kick simplified to only sine.

--- 08/08_kick8.js
+++ 08/08_kick8.js
@@ -6,84 +6,41 @@ window.WH = window.WH || {};
 
 WH.createKick8 = function(specs) {
     var ctx = specs.ctx,
-        noiseSource = specs.noise,
-        voices = [],
-        oscGain,
-        choke,
-        noiseFilter,
-        noiseGain,
-
-        init = function() {
-        	oscGain = ctx.createGain();
-
-            choke = ctx.createGain();
-
-			noiseFilter = ctx.createBiquadFilter();
-			noiseFilter.type = 'bandpass';
-			noiseFilter.frequency.value = 1380 * 2;
-			noiseFilter.Q.value = 20;
-
-            noiseGain = ctx.createGain();
-
-            oscGain.connect(choke);
-            noiseFilter.connect(noiseGain).connect(choke);
-			choke.connect(ctx.destination);
-        },
 
         /**
          * Create an 808 kick drum voice.
          * @return {object} Voice public interface.
          */
-         createVoice = function(when) {
-        	let voice, osc, noise;
-
-        	var tone = 64;
-        	var decay = 64;
-        	var level = 100;
-        	var max = 1;
-			var min = 0.09;
-        	var duration = 0.3; // (max - min) * (decay / 127) + min;
+         createVoice = function(when, duration) {
+        	let osc, gain;
 
         	osc = ctx.createOscillator();
-        	osc.frequency.setValueAtTime(54 + Math.random() * 20, when);
+            gain = ctx.createGain();
+
+            osc.connect(gain).connect(ctx.destination);
+
+        	osc.frequency.setValueAtTime(54, when);
   			osc.frequency.exponentialRampToValueAtTime(32, when + duration);
 
-			oscGain.gain.setValueAtTime(0.0001, when);
-  			oscGain.gain.exponentialRampToValueAtTime(1, when + 0.004);
-  			oscGain.gain.exponentialRampToValueAtTime(0.0001, when + duration);
+			gain.gain.setValueAtTime(0.0001, when);
+  			gain.gain.exponentialRampToValueAtTime(1, when + 0.004);
+  			gain.gain.exponentialRampToValueAtTime(0.0001, when + duration);
 
-            choke.gain.cancelScheduledValues(when);
-			choke.gain.setValueAtTime(0, when);
-			choke.gain.setValueAtTime(0, when + 0.0001);
-			choke.gain.linearRampToValueAtTime(1, when + 0.0002);
+            osc.onended = function(e) {
+                gain.disconnect();
+                gain = null;
+            };
 
-			noise = ctx.createBufferSource();
-			noise.buffer = noiseSource.getWhite();
-			noise.loop = true;
-
-            noiseGain.gain.cancelScheduledValues(when);
-			noiseGain.gain.setValueAtTime(2 * Math.max((tone / 127), 0.0001), when);
-			noiseGain.gain.exponentialRampToValueAtTime(0.0001, when + 0.01);
-
-			osc.connect(oscGain);
-			noise.connect(noiseFilter);
-
-			noise.start(when);
-  			noise.stop(when + duration + 1.0);
-
-			osc.start(when);
-			osc.stop(when + duration + 1.0);
-
-			return voice;
+            osc.start(when);
+            osc.stop(when + duration);
         },
 
         process = function(when, index, length) {
-            createVoice(when);
-            createVoice(when + (length * (2/16)));
-            createVoice(when + (length * (5/16)));
+            createVoice(when, 0.5);
+            createVoice(when + (length * (6/16)), 1.5);
+            createVoice(when + (length * (10/16)), 0.5);
+            createVoice(when + (length * (14/16)), 0.5);
         };
-
-    init();
 
     return {
         process: process
