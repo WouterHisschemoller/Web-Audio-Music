8a729044e9daaab8b6fa9e85282b5f90470296e0
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2017-04-22 15:47:36 +0200

    Click synth sound variation and stereo pan.

--- 08/08_click.js
+++ 08/08_click.js
@@ -6,43 +6,50 @@ window.WH = window.WH || {};
 
 WH.createClick = function(specs) {
     var ctx = specs.ctx,
-        buffer,
+        panner,
+        buffer1,
+        buffer10
 
         init = function() {
             let bufferSize = 100;
-            buffer = ctx.createBuffer(1, bufferSize, ctx.sampleRate);
-            let bufferChannel = buffer.getChannelData(0);
+
+            buffer1 = ctx.createBuffer(1, bufferSize, ctx.sampleRate);
+            let bufferChannel = buffer1.getChannelData(0);
             for (i = 0; i < bufferSize; i++) {
             	bufferChannel[i] = (i > 10 && i < 12) ? 1 : 0;
             }
+
+            buffer10 = ctx.createBuffer(1, bufferSize, ctx.sampleRate);
+            bufferChannel = buffer10.getChannelData(0);
+            for (i = 0; i < bufferSize; i++) {
+            	bufferChannel[i] = (i > 10 && i < 22) ? 1 : 0;
+            }
+
+            panner = ctx.createStereoPanner();
+            panner.connect(ctx.destination);
         },
 
-        createVoice = function(when) {
+        createVoice = function(when, buffer) {
+
+            let pan = (buffer === buffer10) ? -0.7 : 0.2 + (Math.random() * 0.5);
+            panner.pan.setValueAtTime(pan, when);
+
             let src = ctx.createBufferSource();
             src.buffer = buffer;
-            src.connect(ctx.destination);
+            src.connect(panner);
             src.start(when);
         },
 
         process = function(when, index, length) {
-            // createVoice(when + (length * (0/16)));
-            // createVoice(when + (length * (1/16)));
-            // createVoice(when + (length * (2/16)));
-            // createVoice(when + (length * (3/16)));
-            // createVoice(when + (length * (4/16)));
-            createVoice(when + (length * (5/16)));
-            // createVoice(when + (length * (6/16)));
-            // createVoice(when + (length * (7/16)));
-            // createVoice(when + (length * (8/16)));
-            // createVoice(when + (length * (9/16)));
+            createVoice(when + (length * (5/16)), buffer10);
 
-            createVoice(when + (length * (11/16)));
-            createVoice(when + (length * (11.5/16)));
-            createVoice(when + (length * (12/16)));
+            createVoice(when + (length * (11/16)), buffer1);
+            createVoice(when + (length * (11.5/16)), buffer1);
+            createVoice(when + (length * (12/16)), buffer1);
 
-            createVoice(when + (length * (14/16)));
-            createVoice(when + (length * (15/16)));
-            // createVoice(when + (length * (16/16)));
+            createVoice(when + (length * (14/16)), buffer1);
+            createVoice(when + (length * (15/16)), buffer1);
+
         };
 
     init();
