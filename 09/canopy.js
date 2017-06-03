eb1ce00a7b370f6f6b94d5057b9b89dfa5bd9e00
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2017-06-03 19:58:44 +0200

    FM tom sound Canopy snippet.

--- 09/canopy.js
+++ 09/canopy.js
@@ -1,3 +1,58 @@
+// FM Tom
+let ctx = context,
+	when = ctx.currentTime;
+
+let filterFreq = 400,
+    cOsc = ctx.createOscillator(),
+    cGain = ctx.createGain(),
+    m1Osc = ctx.createOscillator(),
+    m1Gain = ctx.createGain(),
+    m2Osc = ctx.createOscillator(),
+    m2Gain = ctx.createGain(),
+    filter1 = ctx.createBiquadFilter(),
+    filter2 = ctx.createBiquadFilter()
+	gain = ctx.createGain();
+
+cOsc.connect(cGain);
+cGain.connect(filter1);
+filter1.connect(filter2);
+filter2.connect(gain);
+gain.connect(ctx.destination);
+m1Osc.connect(m1Gain);
+m1Gain.connect(cOsc.frequency);
+m2Osc.connect(m2Gain);
+m2Gain.connect(m1Osc.frequency);
+
+filter1.type = 'bandpass';
+filter1.frequency.setValueAtTime(filterFreq, when);
+filter1.Q.value = 3;
+
+filter2.type = 'bandpass';
+filter2.frequency.setValueAtTime(filterFreq, when);
+filter2.Q.value = 3;
+
+gain.gain.setValueAtTime(10, when);
+
+cOsc.frequency.setValueAtTime(240.0, when);
+cOsc.frequency.exponentialRampToValueAtTime(199.0, when + 0.003);
+cGain.gain.setValueAtTime(0.7, when);
+cGain.gain.exponentialRampToValueAtTime(0.0001, when + 0.007);
+
+m1Osc.frequency.setValueAtTime(2600.0, when);
+m1Osc.frequency.exponentialRampToValueAtTime(1840.0, when + 0.003);
+m1Gain.gain.setValueAtTime(15000, when);
+m1Gain.gain.exponentialRampToValueAtTime(0.0001, when + 0.007);
+
+m2Osc.frequency.setValueAtTime(159.0, when);
+m2Gain.gain.setValueAtTime(12000, when);
+m2Gain.gain.exponentialRampToValueAtTime(0.0001, when + 0.011);
+
+cOsc.start(when);
+m1Osc.start(when);
+m2Osc.start(when);
+
+
+
 // FM kick tuned in G
 let ctx = context,
 	when = ctx.currentTime;
