41f73204ca6f573d94ad70e2062c46880c235acf
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2017-06-04 12:48:30 +0200

    FM tom tuned in G.

--- 09/09_fmtom.js
+++ 09/09_fmtom.js
@@ -1,5 +1,5 @@
 /**
- * FM tom sound
+ * FM tom sound tuned in G.
  */
 
  window.WH = window.WH || {};
@@ -7,11 +7,12 @@
  WH.createFMTom = function(specs) {
     var ctx = specs.ctx,
         output,
-        filterFreq = 400,
+        filterFreq = 783.991,
 
         /**
          * Create the output stage that voices are connected to.
-         * Two filters in series form a 24dB bandpass filter.
+         * Two filters in series form a 24dB bandpass filter,
+         * followed by makeup gain node.
          */
         init = function() {
             let filter1 = ctx.createBiquadFilter(),
@@ -50,17 +51,17 @@
             m2Osc.connect(m2Gain);
             m2Gain.connect(m1Osc.frequency);
 
-            cOsc.frequency.setValueAtTime(199.0 * 1.2, when);
-            cOsc.frequency.exponentialRampToValueAtTime(199.0, when + 0.003);
+            cOsc.frequency.setValueAtTime(195.998 * 1.2, when);
+            cOsc.frequency.exponentialRampToValueAtTime(195.998, when + 0.003);
             cGain.gain.setValueAtTime(0.7, when);
             cGain.gain.exponentialRampToValueAtTime(0.0001, when + 0.007);
 
-            m1Osc.frequency.setValueAtTime(1840.0 * 1.2, when);
-            m1Osc.frequency.exponentialRampToValueAtTime(1840.0, when + 0.003);
+            m1Osc.frequency.setValueAtTime(1567.982 * 1.2, when);
+            m1Osc.frequency.exponentialRampToValueAtTime(1567.982, when + 0.003);
             m1Gain.gain.setValueAtTime(15000, when);
             m1Gain.gain.exponentialRampToValueAtTime(0.0001, when + 0.007);
 
-            m2Osc.frequency.setValueAtTime(159.0, when);
+            m2Osc.frequency.setValueAtTime(146.832, when);
             m2Gain.gain.setValueAtTime(12000, when);
             m2Gain.gain.exponentialRampToValueAtTime(0.0001, when + 0.011);
 
@@ -83,12 +84,10 @@
         },
 
         process = function(when, index, length) {
-            // if (cOscBuffer) {
-                createVoice(when + (length * (2/16)));
-                createVoice(when + (length * (6/16)));
-                createVoice(when + (length * (8/16)));
-                createVoice(when + (length * (12/16)));
-            // }
+            createVoice(when + (length * (2/16)));
+            createVoice(when + (length * (6/16)));
+            createVoice(when + (length * (8/16)));
+            createVoice(when + (length * (12/16)));
         };
 
     init();
